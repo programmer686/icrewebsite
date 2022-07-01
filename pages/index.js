c14bea5f285bea6bf33b58e947baf390ef665dee
programmer686 <105795650+programmer686@users.noreply.github.com>
2022-07-01 08:02:49 -0500

    hello

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export async function getStaticProps() {
 export default function Home({data}) {
   return (
     
-    <div className="flex justify-center w-screen h-screen bg-red items-center">{data.map(item => (
+    <div className="flex justify-center w-screen h-screen bg-black items-center">{data.map(item => (
       <h1 className="shadow-lg" key={item.id}>{item.title}</h1>
     ))}</div>
     )
