17988fc2bddb15476d9a997d6aa91356920df355
programmer686 <105795650+programmer686@users.noreply.github.com>
2022-07-01 11:40:55 -0500

    the cloud

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,61 @@
-import prisma from '../lib/prisma'
+
+import { headers } from '../next.config'
+import Image from 'next/image';
 
 
 
 
+export default function Home({images}) {
+  
+  const photo = images.map(image => {
+   
+    return (
+      <Image
+      src={image.image}
+      alt=""
+      width={350}
+      height={350}
+      key={image.id} />
+    )
+  })
 
-
-
-export async function getStaticProps() {
-  const movies = await prisma.user.findMany()
-  return {
-    props: {
-      data: movies
-    }
-  } 
-}
-
-
-export default function Home({data}) {
   return (
-    <>
-    <div>{data.map(item =>{
-      <h1>{item.title}</h1>
-      
-    })}</div>
-    
-    </>
+    <div className='bg-black w-auto h-auto p-10 flex justify-center items-center flex-col'>
+      <h1 className='text-white text-[50px] text-center'>Cloudinary<br/> Alojamiento gratuito en la nube para photos y imagenes<br/>es gratis hasta 10GB</h1>
+      <div className=''>{photo}</div>
+    </div>
+        
+        
   )
+}
+  
 
+  export async function getStaticProps() {
+    const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, {
+      headers: {
+        Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
+      }
+    }).then(r => r.json())
+  
+    const { resources } = results;
+
+    const images = resources.map(resource => {
+      
+      return {
+        id: resource.asset_id,
+        title: resource.public_id,
+        image: resource.secure_url,
+        width: resource.width,
+        height: resource.height
+
+      }
+    })
+
+    return {
+      props: {
+        images
+      }
+    }
   }
 
-  export async function getStaticProps()
+  /**/ 
